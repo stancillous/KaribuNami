c76e9c601e979af42b074606cb4c956d8c341262
raymond <stancillousray@gmail.com>
2023-11-01 13:52:23 +0300

    get user location

--- server/flask_api/static/scripts/index.js
+++ server/flask_api/static/scripts/index.js
@@ -1,49 +1,20 @@
 
-// // function getCurrentLocation() {
-// //   navigator.geolocation.getCurrentPosition(function(result) {
-// //     console.log("getting")
-// //     a = result.coords.latitude; // latitude value
-// //     b = result.coords.longitude; // longitude value
+sendForm = document.querySelector(".send-location-dets")
 
-// //     console.log(a)
-// //     console.log(b)
-// //   });
-// // }
-// // geolocationSupported();
-
-// // function geolocationSupported() {
-// //   if (navigator.geolocation) {
-// //     console.log("Geolocation is supported by this browser :)");
-// //     getCurrentLocation();
-// //   } else {
-// //     console.log("Geolocation is NOT supported by this browser :(");
-// //   }
-// // }
+sendForm.addEventListener("submit", (e)=>{
+    e.preventDefault()  // prevent form submit until the geolocation func is called
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const { latitude, longitude } = position.coords;
 
 
+        document.querySelector(".location-lat").value = latitude
+        document.querySelector(".location-long").value = longitude
 
+        console.log(document.querySelector(".location-lat").value)
+        console.log(document.querySelector(".location-long").value)
+        sendForm.submit()   // submit the form
 
+      });
 
-
-// sendForm = document.querySelector(".send-location-dets")
-
-// sendForm.addEventListener("submit", (e)=>{
-//     navigator.geolocation.getCurrentPosition(function (position) {
-//         const { latitude, longitude } = position.coords;
-
-//         console.log("inside")
-//         console.log("lat is ", latitude)
-//         console.log("long is ", longitude)
-      
-//         // Send the geolocation data to your backend
-//         document.querySelector(".location-lat").value = latitude
-//         document.querySelector(".location-long").value = longitude
-
-//         console.log(document.querySelector(".location-lat").value)
-//         console.log(document.querySelector(".location-long").value)
-//         sendForm.submit()
-
-//       });
-
-// })
+})
 
