ec633ba58c62973bbb9bf83b311b2aa8f3ba924d
raymond <stancillousray@gmail.com>
2023-11-02 23:30:11 +0300

    add client side logic

--- server/flask_api/static/scripts/register.js
+++ server/flask_api/static/scripts/register.js
@@ -1,17 +1,17 @@
-// const registerForm = document.querySelector("#signup-form")
+const registerForm = document.querySelector("#signup-form")
 
-// const password = document.querySelector("#user-password").value
-// const confirmPassword = document.querySelector("#confirm-user-password").value
-// registerForm.addEventListener("submit", (e)=>{
-//     // e.preventDefault()
-//     if (password.length < 5) {
-//         e.preventDefault()
-//         document.querySelector(".short-password").classList.add("showFormErrors")
-//     }
-//     if (password != confirmPassword){
-//         e.preventDefault()
-//         document.querySelector(".passwords-mismatch").classList.add("showFormErrors")
-//     }
+const password = document.querySelector("#user-password").value
+const confirmPassword = document.querySelector("#confirm-user-password").value
+registerForm.addEventListener("submit", (e)=>{
+    // e.preventDefault()
+    if (password.length < 5) {
+        e.preventDefault()
+        document.querySelector(".short-password").classList.add("showFormErrors")
+    }
+    if (password != confirmPassword){
+        e.preventDefault()
+        document.querySelector(".passwords-mismatch").classList.add("showFormErrors")
+    }
     
 
-// })
+})
