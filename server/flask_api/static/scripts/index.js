c29eab62516c4d0b0a368246bc41486cfe488cee
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-11-12 21:27:59 +0300

    Fix geolocation api in index.js

--- server/flask_api/static/scripts/index.js
+++ server/flask_api/static/scripts/index.js
@@ -1,22 +1,22 @@
 
 sendForm = document.querySelector(".send-location-dets")
 
-// sendForm.addEventListener("submit", (e)=>{
-//     e.preventDefault()  // prevent form submit until the geolocation func is called
-//     navigator.geolocation.getCurrentPosition(function (position) {
-//         const { latitude, longitude } = position.coords;
+sendForm.addEventListener("submit", (e)=>{
+    e.preventDefault()  // prevent form submit until the geolocation func is called
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const { latitude, longitude } = position.coords;
 
 
-//         document.querySelector(".location-lat").value = latitude
-//         document.querySelector(".location-long").value = longitude
+        document.querySelector(".location-lat").value = latitude
+        document.querySelector(".location-long").value = longitude
 
-//         console.log(document.querySelector(".location-lat").value)
-//         console.log(document.querySelector(".location-long").value)
-//         sendForm.submit()   // submit the form
+        console.log(document.querySelector(".location-lat").value)
+        console.log(document.querySelector(".location-long").value)
+        sendForm.submit()   // submit the form
 
-//       });
+      });
 
-// })
+})
 
 //DYNAMICALLY SET THE YEAR (ON THE FOOTER)
 
