0d00d0a13745cdb721fa4bc7209a321cf3cfebc9
raymond <stancillousray@gmail.com>
2023-11-19 10:04:39 +0300

    fix javascript semistandard errors

--- server/flask_api/static/scripts/index.js
+++ server/flask_api/static/scripts/index.js
@@ -1,36 +1,35 @@
 
-sendForm = document.querySelector(".send-location-dets")
+sendForm = document.querySelector('.send-location-dets');
 
-sendForm.addEventListener("submit", (e)=>{
-    e.preventDefault()  // prevent form submit until the geolocation func is called
-    navigator.geolocation.getCurrentPosition(function (position) {
-        const { latitude, longitude } = position.coords;
+sendForm.addEventListener('submit', (e) => {
+  e.preventDefault(); // prevent form submit until the geolocation func is called
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const { latitude, longitude } = position.coords;
 
-        document.querySelector(".location-lat").value = latitude
-        document.querySelector(".location-long").value = longitude
+    document.querySelector('.location-lat').value = latitude;
+    document.querySelector('.location-long').value = longitude;
 
-        sendForm.submit()   // submit the form
-      },
-      function (error) {
-        if (error.code === error.PERMISSION_DENIED) {
-            // Handle permission denied
-            alert("Please allow location access to submit the form.");
-        }
+    sendForm.submit(); // submit the form
+  },
+  function (error) {
+    if (error.code === error.PERMISSION_DENIED) {
+      // Handle permission denied
+      alert('Please allow location access to submit the form.');
     }
-      );
-})
+  }
+  );
+});
 
-//DYNAMICALLY SET THE YEAR (ON THE FOOTER)
+// DYNAMICALLY SET THE YEAR (ON THE FOOTER)
 
-let currentYear = new Date().getFullYear()
+const currentYear = new Date().getFullYear();
 
-document.querySelector('.cp-year').textContent = currentYear
+document.querySelector('.cp-year').textContent = currentYear;
 
-
-//ANIMATION
-let tl = gsap.timeline()
-tl.from(".tp-ttl", {y:-10, opacity:0, duration:1, ease:"back.easeOut"})
-tl.from(".tp-p", {y:-10, opacity:0, duration:1, ease:"back.easeOut"})
+// ANIMATION
+const tl = gsap.timeline();
+tl.from('.tp-ttl', { y: -10, opacity: 0, duration: 1, ease: 'back.easeOut' });
+tl.from('.tp-p', { y: -10, opacity: 0, duration: 1, ease: 'back.easeOut' });
 
 // gsap.from(".about-app-tag", {
 //   scrollTrigger:{
